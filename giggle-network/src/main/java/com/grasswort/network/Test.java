d511a28c4895aa753c7ac0eb2fd269650c11a272
许良良 <grasswort@qq.com>
2021-01-06 17:04:49 +0800

    crc32 test

---
+++ giggle-network/src/main/java/com/grasswort/network/Test.java
@@ -0,0 +1,12 @@
+package com.grasswort.network;
+
+/**
+ * @author xuliangliang
+ * @Description
+ * @Date 2021/1/6
+ */
+public class Test {
+
+    public static void main(String[] args) {
+    }
+}
